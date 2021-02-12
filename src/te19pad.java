17d3f6ab51280988df1dbf9334470caa65213d6b
Kloska <56342396+Kloska@users.noreply.github.com>
2021-02-12 11:07:34 +0100

    First layout

--- src/te19pad.java
+++ src/te19pad.java
@@ -2,4 +2,17 @@ import javax.swing.*;
 
 public class te19pad extends JFrame {
     private JPanel mainPanel;
+    private JPanel panel1;
+    private JTextPane textPane1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("te19pad");
+        frame.setContentPane(new te19pad().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
